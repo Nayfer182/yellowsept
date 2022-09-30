67defb2e1e962e9b571a000eb20f46de0f3e913a
Bruno Duarte <100389894+Nayfer182@users.noreply.github.com>
2022-09-29 22:18:01 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import { Text, View, StyleSheet,Image,TextInput,TouchableOpacity, Alert } from "react-native";
+import Cartilha from './components/Cartilha'
+
+export default function App(){
+  return(
+   <View>
+    <Cartilha/>
+   </View>
+  );
+}
